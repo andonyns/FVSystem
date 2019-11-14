8bf92ae93f7a3eda39f60d11b66d69d7cf9214c0
Andony Nunez <Andony.Nunez@abrigo.com>
2019-11-14 16:08:48 -0600

    Test

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,7 +5,6 @@ namespace FVSystem.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
