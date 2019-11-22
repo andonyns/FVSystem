de36f9b64a91ee2a6c249f877430e27b2713be63
Andony Nuñez <andony91@gmail.com>
2019-11-21 18:40:52 -0600

    Creando la pagina nueva para aasignar notas

---
+++ Controllers/NotasController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FVSystem.Controllers
+{
+    public class NotasController : Controller
+    {
+        public IActionResult Asignar()
+        {
+            return View();
+        }
+    }
+}
