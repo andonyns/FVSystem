dd295eaf1bcdf58b3fb3ee33a6232ac25ab85095
Estudiante <ci25julio@gmail.com>
2019-11-21 20:57:27 -0600

    Mostar nombre del estudiante al que le voy asignar notas

--- Controllers/NotasController.cs
+++ Controllers/NotasController.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FVSystem.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace FVSystem.Controllers
 {
     public class NotasController : Controller
     {
-        public IActionResult Asignar()
+        private EstudiantesRepository repository;
+
+        public NotasController(IConfiguration config)
         {
-            return View();
+            repository = new EstudiantesRepository(config);
+        }
+
+        public IActionResult Asignar(string id)
+        {
+
+
+            var estudiante = repository.ObtenerEstudiante(id);
+
+            return View(estudiante);
+
         }
     }
 }
