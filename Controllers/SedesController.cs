36ca10d060bc0d24c9a3c04bafc4dd79ca07cb75
deiverherrerars@outlook.com <deiverherrerars@outlook.com>
2019-11-26 19:51:43 -0600

    Agrege una nueva pagina cambios en css

---
+++ Controllers/SedesController.cs
@@ -0,0 +1,28 @@
+﻿using FVSystem.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FVSystem.Controllers
+{
+    public class SedesController : Controller
+    {
+        private IConfiguration configuration;
+        private SedesRepository repository;
+
+        public SedesController(IConfiguration config)
+        {
+            configuration = config;
+            repository = new SedesRepository();
+        }
+        public IActionResult Index()
+        {
+            var sedes = repository.GetSedes();
+
+            return View(sedes);
+        }
+    }
+}
