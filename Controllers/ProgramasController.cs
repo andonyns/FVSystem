f84914d7a0a4ad4a65a2c1cff87dc8a9673ad7ef
Andony Nuñez <andony91@gmail.com>
2019-12-02 18:09:13 -0600

    Cambios para que el codigo compile, cambios al archivo docker

--- Controllers/ProgramasController.cs
+++ Controllers/ProgramasController.cs
@@ -3,60 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace FVSystem.Controllers
 {
     public class ProgramasController : Controller
     {
-        private ProgramasRepository;
-        private CursosRepository cursosRepository;
+        //private ProgramasRepository repository;
+        //private CursosRepository cursosRepository;
 
-        public ModuloController(IConfiguration config)
+        public ProgramasController(IConfiguration config)
         {
-            repository = new ProgramasRepository(config);
-            cursosRepository = new CursosRepository(config);
-            
-            
+            //repository = new ProgramasRepository(config);
+            //cursosRepository = new CursosRepository(config);
+
+        }
             public IActionResult Index()
         {
-                var programas = repository.ObtenerProgramas();
-                if (cursos == null || cursos.Count == 0)
-                {
-                    ViewBag.ErrorMessage = "No se encontraron programas";
-                }
+            //var programas = repository.ObtenerProgramas();
+            //if (cursos == null || cursos.Count == 0)
+            //{
+            //    ViewBag.ErrorMessage = "No se encontraron programas";
+            //}
 
-                return View("Lista", programas);
+            //return View("Lista", programas);
 
-                return View();
+            return View();
         }
 
             // GET: Modulo
             public ActionResult Agregar()
             {
-                var listaProgramas = programasRepository.ObtenerProgramas();
+                var listaProgramas = 2;
+                // programasRepository.ObtenerProgramas();
 
                 return View(listaProgramas);
             }
 
-            public ActionResult Editar(string id)
-            {
-                var programas = repository.ObtenerProgramas(id);
+            //public ActionResult Editar(string id)
+            //{
+            //    var programas = repository.ObtenerProgramas(id);
 
-                return View(programas);
-            }
+            //    return View(programas);
+            //}
 
-            public ActionResult Guardar(Programas programas)
-            {
-                repository.InsertarProgramas(programas);
+            //public ActionResult Guardar(Programas programas)
+            //{
+            //    repository.InsertarProgramas(programas);
 
-                return Redirect("/Programas");
-            }
+            //    return Redirect("/Programas");
+            //}
 
-            public ActionResult Actualizar(Programas programas)
-            {
-                repository.ActualizarProgramas(programas);
+            //public ActionResult Actualizar(Programas programas)
+            //{
+            //    repository.ActualizarProgramas(programas);
 
-                return Redirect("/Programas");
-            }
+            //    return Redirect("/Programas");
+            //}
         }
 }
--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,20 @@
-FROM mcr.microsoft.com/dotnet/core/aspnet:3.0-buster-slim AS base
-WORKDIR /app
-EXPOSE 80
-EXPOSE 443
-
-FROM mcr.microsoft.com/dotnet/core/sdk:3.0-buster AS build
+# NuGet restore
+FROM mcr.microsoft.com/dotnet/core/sdk:3.0 AS build
 WORKDIR /src
-COPY ["FVSystem.csproj", ""]
-RUN dotnet restore "./FVSystem.csproj"
+COPY *.sln .
+COPY FVSystem/*.csproj FVSystem/
+RUN dotnet restore
 COPY . .
-WORKDIR "/src/."
-RUN dotnet build "FVSystem.csproj" -c Release -o /app/build
 
+
+# publish
 FROM build AS publish
-RUN dotnet publish "FVSystem.csproj" -c Release -o /app/publish
+WORKDIR /src/FVSystem
+RUN dotnet publish -c Release -o /src/publish
 
-FROM base AS final
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.0 AS runtime
 WORKDIR /app
-COPY --from=publish /app/publish .
-ENTRYPOINT ["dotnet", "FVSystem.dll"]
+COPY --from=publish /src/publish .
+
+# heroku uses the following
+CMD ASPNETCORE_URLS=http://*:$PORT dotnet FVSystem.dll
